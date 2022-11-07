30351b8e3704f669ecdc1015afbfaa86ec197c12
Khairul2991 <auni3934@gmail.com>
2022-11-07 22:38:07 +0700

    penambahan isi

--- pelanggan/Pelanggan.java
+++ pelanggan/Pelanggan.java
@@ -1,5 +1,31 @@
 package pelanggan;
 
 public class Pelanggan {
-    System.out.println("Hello World!!");
+    private String nama;
+    private Menu order[];
+
+    public Pelanggan 
+    {
+        this.nama = nama;
+    }
+
+    public String getNama() 
+    {
+        return this.nama;
+    }
+
+    public void setNama(String nama)
+    {
+        this.nama = nama;
+    }
+
+    public void addOrder(Menu pesan)
+    {
+
+    }
+
+    public Menu getOrder()
+    {
+
+    }
 }
