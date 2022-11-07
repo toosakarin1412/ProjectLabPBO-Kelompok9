10fa6a0a06c489e99a4d1973df1e03dfef91e4a8
toosakarin1412 <toosakarin1412@gmail.com>
2022-11-08 01:04:17 +0700

    update menu

---
+++ menu/AirMineral.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class AirMineral extends Minuman {
+    
+    public AirMineral(){
+        this.nama = "AirMineral";
+        this.harga = 3000;
+    };
+}
---
+++ menu/Jus.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class Jus extends Minuman {
+
+    public Jus(String nama, int harga){
+        this.nama = nama;
+        this.harga = harga;
+    };
+}
