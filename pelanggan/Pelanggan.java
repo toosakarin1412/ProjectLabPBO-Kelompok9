8f986f71a302559f80946ded2afa33500d24a3e5
Khairul2991 <116780621+Khairul2991@users.noreply.github.com>
2022-11-08 23:10:55 +0700

    Update Pelanggan.java
    Penambahan javadoc

--- pelanggan/Pelanggan.java
+++ pelanggan/Pelanggan.java
@@ -6,30 +6,51 @@ public class Pelanggan {
     private String nama;
     private ArrayList<Menu> order;
 
+    /**
+    * Method Constructor
+    */
     public Pelanggan(){
 
     };
 
+    /**
+    * Method Constructor
+    * @param nama pelanggan
+    */
     public Pelanggan(String nama) 
     {
         this.nama = nama;
     }
 
+    /**
+    * Mengembalikan nama dari pelanggan
+    * @return this.nama nama pelanggan
+    */
     public String getNama() 
     {
         return this.nama;
     }
 
+    /**
+    * Mengatur nama pelanggan
+    * @param nama nama pelanggan
+    */
     public void setNama(String nama)
     {
         this.nama = nama;
     }
 
+    /**
+    * @param pesan pesan menu
+    */
     public void addOrder(Menu pesan)
     {
         order.add(pesan);
     }
 
+    /**
+    * @return this.order
+    */
     public ArrayList<Menu> getOrder()
     {
         return this.order;
