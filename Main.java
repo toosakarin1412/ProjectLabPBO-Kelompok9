c98e8d3340c28ed8668a27239349de9aead6c716
toosakarin1412 <toosakarin1412@gmail.com>
2022-11-08 23:49:03 +0700

    tambah method dan menu apk

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import menu.*;
 import pelanggan.*;
 import menu.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     private ArrayList<Menu> daftar_menu;
@@ -18,6 +20,24 @@ public class Main {
      * Baca Menu
      */
     public void readMenu(){
+
+        // Implementasi
+
+    };
+
+    /**
+     * Tampilkan menu
+     */
+    public void printMenu(){
+
+        // Implementasi
+
+    };
+
+    /**
+     * Tampilkan pelanggan
+     */
+    public void printPelanggan(){
 
         // Implementasi
 
@@ -45,6 +65,43 @@ public class Main {
     };
     
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Main driver = new Main();
         
+        while(true){
+            System.out.println("==============================");
+            System.out.println("APLIKASI KASIR RESTORAN XXX");
+            System.out.println("==============================");
+            System.out.println("1.MENU");
+            System.out.println("2.PELANGGAN");
+            System.out.println("3.HITUNG TAGIHAN");
+            System.out.println("9.EXIT");
+            System.out.println("==============================");
+
+            int pilihan;
+            try{
+                pilihan = input.nextInt();
+            }catch(InputMismatchException e){
+                input.nextLine();
+                continue;
+            };
+
+            if(pilihan == 1){
+                driver.printMenu();
+            }else if(pilihan == 2){
+                System.out.println("==============================");
+                System.out.println("PELANGGAN");
+                System.out.println("==============================");
+            }else if(pilihan == 3){
+                System.out.println("==============================");
+                System.out.println("TAGIHAN");
+                System.out.println("==============================");
+            }else if(pilihan == 9){
+                break;
+            }else{
+                System.out.println("INPUT TIDAK SESUAI MENU");
+                continue;
+            }
+        };
     };
 }
